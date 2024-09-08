2d76d0f556be92179e5bd9e57b49aa080a378361
AbedAlmajed <abedalmajedalajarmah@gmail.com>
2024-09-08 16:14:19 +0300

    added manage menu and delete item from menu

--- Admin/backend/routes/menuItemRoutes.js
+++ Admin/backend/routes/menuItemRoutes.js
@@ -1,8 +1,21 @@
-// routes/menuItemRoutes.js
+// // routes/menuItemRoutes.js
+// const express = require('express');
+// const router = express.Router();
+// const menuItemController = require('../controllers/menuItemController');
+
+// router.post('/menu-items', menuItemController.createMenuItem);
+
+// module.exports = router;
+
+
+
 const express = require('express');
 const router = express.Router();
 const menuItemController = require('../controllers/menuItemController');
 
 router.post('/menu-items', menuItemController.createMenuItem);
+router.get('/menu-items', menuItemController.getMenuItems);
+router.delete('/menu-items/:id', menuItemController.deleteMenuItem);
+router.put('/menu-items/:id', menuItemController.updateMenuItem);
 
 module.exports = router;
